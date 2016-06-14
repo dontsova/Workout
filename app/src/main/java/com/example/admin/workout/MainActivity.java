8b5ef7c9290a378cb2ffc6846c165022dc8b74b1
dontsova <v.koroliev@gmail.com>
2016-06-14 17:44:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/admin/workout/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.admin.workout;
+
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity implements WorkoutListFragment.WorkoutListListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        WorkoutDetailFragment details = new WorkoutDetailFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        details.setWorkout(id);
+        ft.replace(R.id.fragment_container, details);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+    }
+}
